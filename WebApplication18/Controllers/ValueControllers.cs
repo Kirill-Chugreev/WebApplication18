7a6940b0d0ea8ac7dc97810c4137e9abf4fcca23
Chugreev Kirill <wern587@gmail.com>
2024-05-31 19:13:26 +0300

    Добавьте файлы проекта.

---
+++ WebApplication18/Controllers/ValueControllers.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication18.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRandomNumber()
+        {
+            int number = new Random().Next(1, 100);
+            Task.Delay(1000);
+            return Ok(number);
+        }
+
+        [HttpPost]
+        public IActionResult GetResult()
+        {
+            return Ok(new { message = "Hello world" });
+        }
+
+        [HttpDelete]
+        public IActionResult GetError(int id)
+        {
+            if (id == 1) throw new Exception();
+            return BadRequest();
+        }
+    }
+}
